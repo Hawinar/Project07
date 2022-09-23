a1506112b53c38c471e225d72d2116f65c7fca32
Hawinar <egorchelnokov@bk.ru>
2022-09-23 20:36:07 +0300

    Добавьте файлы проекта.

---
+++ Project07/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace Project07
+{
+    /// <summary>
+    /// 3.2	Лабораторная работа № 3. Тема: «Модульное проектирование ПС» Вариант №2 https://github.com/Hawinar
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DGridStudents.ItemsSource = Project07TestEntities.GetContext().Student.ToList();
+        }
+    }
+}
